21ab6df7753ac6b5b56b92751b7af190c3e9eb33
Minh Khoa <88275892+IAmMinhKhoa@users.noreply.github.com>
2023-08-08 16:44:19 +0700

    1

---
+++ Assets/Popipa/Character_1/Script/Player1Controller.cs
@@ -0,0 +1,94 @@
+using UnityEngine;
+using System.Collections;
+
+
+public class Player1Controller : MonoBehaviour
+{
+    public float speed = 5f;
+    public Animator anim;
+	private Rigidbody2D rb;
+    public bool facingRight;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	// Controls facing direction
+	
+	 
+	public void Jump()
+    {
+		anim.SetBool("Jump", true) ;
+	}
+
+	public void JumpOff()
+    {
+		anim.SetBool("Jump", false);
+	}
+
+	public void Dead()
+	{
+		anim.SetBool("Dead" , true);
+	}
+
+	public void DeadOff()
+	{
+		anim.SetBool("Dead", false);
+	}
+	public void Walk()
+	{
+		anim.SetBool("Walk" , true);
+	}
+
+	public void WalkOff()
+	{
+		anim.SetBool("Walk", false);
+	}
+	public void Run()
+	{
+		anim.SetBool("Run" , true);
+	}
+	public void RunOff()
+	{
+		anim.SetBool("Run", false);
+	}
+	public void Attack()
+	{
+		anim.SetBool("Attack" , true);
+	}
+	public void AttackOff()
+	{
+		anim.SetBool("Attack", false);
+	}
+
+	void Update()
+	{
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+		if (moveHorizontal != 0)
+		{
+			Walk();
+		}
+		else
+		{
+			WalkOff();
+
+        }
+        if (moveVertical != 0)
+        {
+            Run();
+        }
+        else
+        {
+            RunOff();
+
+        }
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        rb.velocity = movement * speed;
+		Debug.Log(moveHorizontal);
+    } 
+
+
+}
+
+
+
