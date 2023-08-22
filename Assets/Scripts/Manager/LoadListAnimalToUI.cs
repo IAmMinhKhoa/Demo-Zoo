691a46714314a5404c01f36fcbc221506b09c0b5
Minh Khoa <88275892+IAmMinhKhoa@users.noreply.github.com>
2023-08-22 18:08:05 +0700

    8

--- Assets/Scripts/Manager/LoadListAnimalToUI.cs
+++ Assets/Scripts/Manager/LoadListAnimalToUI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
@@ -6,56 +7,78 @@ using static SO_ListAnimal;
 
 public class LoadListAnimalToUI : MonoBehaviour
 {
-    public SO_ListAnimal listAnimal;
+
+
+ 
+
+
+
+    public enum NameZone { Forest }
+    [SerializeField] NameZone nameZone;
+    protected List<string> ListNameZone = new List<string>();
     public GameObject prefabCardAnimal;
-    [Header("Need to enter NameZone(Environment Name)")]
-    public string NameZone;
-    [Header("Need Drag Area_Map_'NameZone' of that environment")]
-    public GameObject GO_AreaMap; //GO=GameObject
-    protected GameObject GO_ListAnimal;
+    public SO_ListAnimal listAnimal;
+ 
+
     private void Start()
     {
-        LoadData();
-        
+        LoadData("Forest");
+        //LoadDataListNameZone();
+
     }
-    protected void LoadData()
+    protected void LoadDataListNameZone()
+    {
+        foreach (AnimalZone animalZone in listAnimal.List_Animal_Zone)
+        {
+            ListNameZone.Add(animalZone.NameZone);
+
+        }
+    }
+
+ 
+
+
+
+    protected void LoadData(String name)
     {
         bool checkFindZone=false;
-        GO_ListAnimal = FindChildObject(GO_AreaMap, "List Animal");
+        GameObject GO_ListAnimal = FindChildObject(this.gameObject, "List Animal");
         
         foreach (AnimalZone ListZone in listAnimal.List_Animal_Zone)
         {
-            if(ListZone.NameZone == NameZone)
+            if(ListZone.NameZone == name)
             {
                 checkFindZone = true;
                 foreach  (SO_Animal ListDataAnimal in ListZone.DATA_Animals)
                 {
-                   GameObject CardAnimal = Instantiate(prefabCardAnimal);
-                    CardAnimal.GetComponent<ComponentCardAnimalInBook>().ChangeIcon(ListDataAnimal.Icon);
-                    CardAnimal.GetComponent<ComponentCardAnimalInBook>().ChangeName(ListDataAnimal.NameVn, ListDataAnimal.NameUS);
-                    CardAnimal.transform.SetParent(GO_ListAnimal.transform);
+                    InstanceCardAnimal(GO_ListAnimal,ListDataAnimal);
                 }
             }
         }
         if (checkFindZone != true)
         {
-            Debug.LogWarning("Not Finding this " + NameZone);
+            Debug.LogWarning("Not Finding this " + name);
         }
     }
 
+    protected void InstanceCardAnimal(GameObject GO_ListAnimal,SO_Animal ListDataAnimal)
+    {
+        GameObject CardAnimal = Instantiate(prefabCardAnimal);
+        CardAnimal.transform.SetParent(GO_ListAnimal.transform);
+        CardAnimal.transform.localScale = new Vector3(1, 1, 1);
+        CardAnimal.GetComponent<ComponentCardAnimalInBook>().ChangeIcon(ListDataAnimal.Icon);
+        CardAnimal.GetComponent<ComponentCardAnimalInBook>().ChangeName(ListDataAnimal.NameVn, ListDataAnimal.NameUS);
+    }
 
     protected GameObject FindChildObject(GameObject parent,string name)
     {
         Transform childObject = parent.transform.Find(name);
         if (childObject != null)
         {
-            // ?ã tìm th?y ??i t??ng con có tên "a"
-            // Th?c hi?n các thao tác khác v?i childObject
             return childObject.gameObject;
         }
         else
         {
-            // Không tìm th?y ??i t??ng con có tên "a"
             return null;
         }
     }
